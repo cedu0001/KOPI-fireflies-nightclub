1d14cf01e6208b026e3f1c5999dc5915f1286ad2
Celia <cmarianielsen@gmail.com>
2026-05-08 13:33:08 +0200

    Færdig med hero til index + responsiv

---
+++ src/components/HeroIndex.jsx
@@ -0,0 +1,23 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+
+const HeroIndex = () => {
+	return (
+		<section className="hero-index full-width flex flex-col gap-3 items-center p-(--space-3xl) sm:py-50  ">
+			<div className="mt-auto mb-auto ">
+				<Image src="/assets/icon/Logo.svg" alt="Logo" width={563} height={63} />
+				<Image src="/assets/bottom_line2.png" alt="Logo" width={563} height={63} />
+				<div className="flex justify-center gap-5">
+					<button className="bg-black/50 min-w-[160px] block mt-4 relative to-highlight-accent p-4 pr-6 pl-6 border-(--sidebar-ring) border-2 cursor-pointer after:absolute after:top-0 after:right-0 after:w-5 after:h-5 after:border-t-2 after:border-r-2 after:border-primary-foreground/50 after:opacity-100 after:blur-[1px]">
+						VIEW EVENTS
+					</button>
+					<Button variant="highlight" className="min-w-[160px] block mt-4">
+						BOOK TABLE
+					</Button>
+				</div>
+			</div>
+		</section>
+	);
+};
+
+export default HeroIndex;
