f3f27215b773e7da49124ad43d75ddb5d234620a
Jerie <jerie.stolbergrohr@gmail.com>
2026-05-19 19:30:04 +0200

    selected names are back!

--- src/components/TableBookingForm.jsx
+++ src/components/TableBookingForm.jsx
@@ -3,6 +3,7 @@
 import Form from "next/form";
 import { InputBasic } from "./ui/input";
 import { Button } from "./ui/button";
+import { SelectTrigger, SelectContent, SelectItem, SelectValue, Select } from "./ui/select";
 const TableBookingForm = ({events, selectedEvent, setSelectedEvent, selectedTable, setSelectedTable, reservations}) => {
     const isReserved = (tableNumber) => {
         if(!selectedEvent) return false;
@@ -13,6 +14,18 @@ const TableBookingForm = ({events, selectedEvent, setSelectedEvent, selectedTabl
     Number(reservation.table) === tableNumber
   );
 };
+const selectedEventTitle =
+  events.find(
+    (event) =>
+      String(event.id) ===
+      String(selectedEvent)
+  )?.title;
+
+  const selectedTableLabel =
+  selectedTable
+    ? `Table ${selectedTable}`
+    : "";
+
     return ( 
         <Form className="mb-16">
             <h2>BOOK A TABLE</h2>
@@ -21,19 +34,24 @@ const TableBookingForm = ({events, selectedEvent, setSelectedEvent, selectedTabl
             className="max-w-148 min-w-48"/>
             <InputBasic type="email" placeholder="Your Email"
             className="max-w-148 min-w-48"/>
-            <select
+
+            <Select
             name="table"
             value={selectedTable}
-            onChange={(e) => setSelectedTable(Number(e.target.value))}
-             className="w-full border border-input bg-transparent px-2.5 py-4 max-w-148 min-w-48 text-text">
-            <option value="">
-            Select Table
-            </option>
+            onValueChange={(value) => setSelectedTable(Number(value))}
+             className="w-full border border-input bg-transparent max-w-148 min-w-48 text-text">
+            <SelectTrigger
+            className="w-full  max-w-148 min-w-48">
+            <SelectValue>
+            {selectedTableLabel || "Select Table"}
+            </SelectValue>
+            </SelectTrigger>
+            <SelectContent>
   {Array.from({ length: 15 }).map(
     (_, index) => {
         const tableNumber = index + 1;
       return (
-      <option
+      <SelectItem
         key={tableNumber}
         value={tableNumber}
         disabled={isReserved(tableNumber)}
@@ -42,36 +60,41 @@ const TableBookingForm = ({events, selectedEvent, setSelectedEvent, selectedTabl
         {isReserved(tableNumber)
           ? " (Reserved)"
           : ""}
-      </option>
+      </SelectItem>
       );
     }
   )}
-</select>
+  </SelectContent>
+</Select>
             <InputBasic type="number" placeholder="Number Of Guests"
             className="max-w-148 min-w-48"/>
-            <select
+          
+            <Select
             name="eventId"
-            value={selectedEvent}
-            onChange={(e) => setSelectedEvent(e.target.value)}
+            value={String(selectedEvent)} 
+            onValueChange={(value) => setSelectedEvent(value)}
             className="
             w-full border border-input
             bg-transparent px-2.5 py-4
             max-w-148 min-w-48 mb-4
             text-text"      >
-
-          <option value="">
-            Select Event
-          </option>
-
+                <SelectTrigger className="w-full max-w-148 min-w-48">
+                    <SelectValue>
+                    {selectedEventTitle ||
+                    "Select Event"}
+                    </SelectValue>
+                </SelectTrigger>
+            <SelectContent>
           {events.map((event) => (
-            <option
+            <SelectItem
               key={event.id}
-              value={event.id}
+              value={String(event.id)}
             >
               {event.title}
-            </option>
+            </SelectItem>
           ))}
-        </select>
+          </SelectContent>
+        </Select>
             <InputBasic type="tel" placeholder="Your Telephone Number"
             className="max-w-148 min-w-48"/>
             </section>
