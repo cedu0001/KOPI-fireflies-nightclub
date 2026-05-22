17231fd781f905c4a20481d18169496f35e7bfb9
Celia <cmarianielsen@gmail.com>
2026-05-22 15:32:46 +0200

    færdig med newsletters post, lille styling til den globale nav bar

--- src/components/Newsletter.jsx
+++ src/components/Newsletter.jsx
@@ -1,7 +1,15 @@
+"use client";
 import Form from "next/form";
 import { Button } from "./ui/button";
+import { useActionState } from "react";
+
+import newsletterPost from "@/components/newsletterPost";
 
 const Newsletter = () => {
+	const [state, formAction, isPending] = useActionState(newsletterPost, {
+		success: false,
+		message: "",
+	});
 	return (
 		<section className="flex flex-col gap-3 items-center my-(--space-3xl)">
 			<div className="text-center pb-(--space-xl)">
@@ -10,15 +18,18 @@ const Newsletter = () => {
 					Subscribe to our newsletter and never miss an <span className="text-(--highlight-secondary)">Event</span>
 				</p>
 			</div>
-			<Form className="flex flex-col sm:flex-row gap-10">
+			<Form action={formAction} className="flex flex-col sm:flex-row gap-10">
+				{state?.message && (
+					<div className={`border p-4 ${state.success ? "" : "border-red-500 text-red-500"}`}>{state.message}</div>
+				)}
 				<input
-					name="resname"
+					name="email"
 					className="w-[353px] lg:w-[500px] placeholder-primary-foreground p-(--space-2xs) text-left border-b-2 bg-primary text-primary-foreground border-primary-foreground [a]:hover:bg-primary/80 aria-expanded:bg-primary aria-expanded:text-primary-foreground"
-					type="text"
+					type="email"
 					placeholder="Enter your email"
 				/>
-				<Button variant="secondary" className="w-[173px] mr-auto ml-auto">
-					Submit
+				<Button variant="secondary" type="submit" className="w-[173px] mr-auto ml-auto">
+					{isPending ? "Sending..." : "SEND"}
 				</Button>
 			</Form>
 		</section>
