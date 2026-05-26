e7a944f19fadbcbd668fedf45ca48489e717d872
Celia <cmarianielsen@gmail.com>
2026-05-26 09:42:33 +0200

    færdig med at rette og tilføje

--- src/app/events/[slug]/page.js
+++ src/app/events/[slug]/page.js
@@ -1,17 +1,9 @@
 import SingleEventServer from "@/components/events/SingleEventServer";
 import { Suspense } from "react";
 
-export async function generateMetadata({ params }) {
-	const { slug } = await params;
-
-	const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/events`);
-	const data = await response.json();
-
-	const event = data.find((event) => event.slug === slug);
-	return {
-		title: `NightClub | ${event.title}`,
-	};
-}
+export const metadata = {
+	title: "NightClub",
+};
 
 export default async function SingleEventSite({ params }) {
 	return (
