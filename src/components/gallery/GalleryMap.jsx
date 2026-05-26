1d95a7006fb68ff0cd25e81cd69047c5cd14e81a
Celia <cmarianielsen@gmail.com>
2026-05-26 10:15:58 +0200

    fikset den sidste overskift

--- src/components/gallery/GalleryMap.jsx
+++ src/components/gallery/GalleryMap.jsx
@@ -3,28 +3,25 @@ import Headline from "@/components/Headline";
 import { cacheLife } from "next/cache";
 
 const GalleryMap = async () => {
-  "use cache";
-  cacheLife("hours");
+	"use cache";
+	cacheLife("hours");
 
-  const response = await fetch(
-    `${process.env.NEXT_PUBLIC_API_URL}/gallery?_limit=6`,
-  );
+	const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/gallery?_limit=6`);
 
-  const data = await response.json();
+	const data = await response.json();
 
-  const formattedData = data.map((gallery) => ({
-    ...gallery,
-    imageUrl:
-      `${process.env.NEXT_PUBLIC_API_URL}${gallery.asset?.url}`,
-  }));
+	const formattedData = data.map((gallery) => ({
+		...gallery,
+		imageUrl: `${process.env.NEXT_PUBLIC_API_URL}${gallery.asset?.url}`,
+	}));
 
-  return (
-    <article className="full-width my-22">
-      <Headline title="GALLERY" />
+	return (
+		<article className="full-width my-22">
+			<Headline title="NIGHT CLUB GALLERY" />
 
-      <GalleryClient galleryData={formattedData} />
-    </article>
-  );
+			<GalleryClient galleryData={formattedData} />
+		</article>
+	);
 };
 
 export default GalleryMap;
